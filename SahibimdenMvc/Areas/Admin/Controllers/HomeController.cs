924764c1bbdc3480289d84af15bcc5bb0af84c9a
Emre ALABAS <emre.alabas@outlook.com>
2020-05-12 10:57:27 +0300

    Area ve admin girişi eklendi

---
+++ SahibimdenMvc/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using SahibimdenMvc.Areas.Admin.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SahibimdenMvc.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        [SessionControl]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SahibimdenMvc/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using SahibimdenMvc.Models.AdminModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace SahibimdenMvc.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult LoginControl(LoginModel model, string returnurl)
+        {
+            if (ModelState.IsValid)
+            {
+                //Aşağıdaki if komutu gönderilen mail ve şifre doğrultusunda kullanıcı kontrolu yapar. Eğer kullanıcı var ise login olur.
+                if (model.EMail.ToLower() == "emre" && model.Password.ToLower() == "123")
+                {
+                    FormsAuthentication.SetAuthCookie(model.EMail, true);
+                    return RedirectToAction("Index", "Home");
+                }
+
+                else
+                {
+                    ModelState.AddModelError("", "EMail veya şifre hatalı!");
+                }
+            }
+            return View("Index", model);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index");
+        }
+    }
+}
