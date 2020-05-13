c044bcc34a761c7da02e0d08e21325c01a2aa304
Emre ALABAS <emre.alabas@outlook.com>
2020-05-13 16:35:16 +0300

    Anasayfaya duyurular menüsü eklendi

--- SahibimdenMvc/Controllers/DuyuruController.cs
+++ SahibimdenMvc/Controllers/DuyuruController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SahibimdenMvc.DAL;
+using SahibimdenMvc.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,27 @@ namespace SahibimdenMvc.Controllers
 {
     public class DuyuruController : Controller
     {
+        SahibimdenContext ctx;
         // GET: Duyuru
         public ActionResult Index()
         {
-            return View();
+            using (ctx = new SahibimdenContext())
+            {
+                List<Duyuru> duyurular = new List<Duyuru>();
+                duyurular = ctx.Duyurular.ToList();
+
+                return View(duyurular);
+            }          
+        }
+
+        [HttpPost]
+        public string DuyuruGoruntule(int? id)
+        {
+            using (SahibimdenContext ctx = new SahibimdenContext())
+            {
+                Duyuru duyuru = ctx.Duyurular.Find(id);
+                return duyuru.Mesaj;
+            }
         }
     }
 }
